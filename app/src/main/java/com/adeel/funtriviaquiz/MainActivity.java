75612fea9ea2a605d5a8147b7b533c7d34e4bbaf
adeelsohailahmed <i_am_cool_boy@live.com>
2020-08-13 10:09:36 +0500

    tweaked a little with layout

--- app/src/main/java/com/adeel/funtriviaquiz/MainActivity.java
+++ app/src/main/java/com/adeel/funtriviaquiz/MainActivity.java
@@ -3,6 +3,7 @@ package com.adeel.funtriviaquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void exit (View view) {
+        finish(); // Close the app.
     }
 }
