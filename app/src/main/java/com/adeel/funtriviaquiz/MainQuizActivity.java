3b5f2c53780e6594247b22e8008c816102e94212
adeelsohailahmed <i_am_cool_boy@live.com>
2020-08-16 21:20:22 +0500

    question and options are now fetched from local json file; selected answer will be compared with correct answer; sound effect plays depending on what answer was selected; correct answer will add 10 to the total score; added the correct way to concatenate the question number;

--- app/src/main/java/com/adeel/funtriviaquiz/MainQuizActivity.java
+++ app/src/main/java/com/adeel/funtriviaquiz/MainQuizActivity.java
@@ -2,7 +2,11 @@ package com.adeel.funtriviaquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -11,9 +15,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
 import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
@@ -26,21 +35,19 @@ public class MainQuizActivity extends AppCompatActivity {
     TextView textViewQuestionNumber;
     TextView textViewQuestionDescription;
 
+    String correctAnswer;
+    int questionNumber, totalScore;
+
+    MediaPlayer soundClip;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_quiz);
 
+        // Get references to Question Number and Question Description
         textViewQuestionNumber = (TextView) findViewById(R.id.textViewQuestionNumber);
         textViewQuestionDescription = (TextView) findViewById(R.id.textViewQuestionDescription);
-
-        textViewQuestionNumber.setText(textViewQuestionNumber.getText().toString() + "1");
-
-        // TODO: Read from json asset file and parse it with json object.
-        // TODO: Set description and answers via json object.
-
-
-        textViewQuestionDescription.setText("What is the capital of France?");
 
         // Get references of all four radio buttons so that their text may be updated dynamically
         radioButton = (RadioButton) findViewById(R.id.radioButton);
@@ -48,11 +55,45 @@ public class MainQuizActivity extends AppCompatActivity {
         radioButton3 = (RadioButton) findViewById(R.id.radioButton3);
         radioButton4 = (RadioButton) findViewById(R.id.radioButton4);
 
-        // Set the text of all four radio buttons with the answers obtained from JSON File
-        radioButton.setText("France");
-        radioButton2.setText("London");
-        radioButton3.setText("Rome");
-        radioButton4.setText("Belgium");
+        // Initialize the question number and total score since this is the first run
+        questionNumber = 1;
+        totalScore = 0;
+
+        // Get current question number and append it to the question number textView
+        textViewQuestionNumber.setText(getString(R.string.question_number, questionNumber));
+
+        // TODO: Add more questions to JSON file. At least 50 questions to keep things interesting.
+        // TODO: Get randomized 10 questions from the JSON file and display to user.
+        // TODO: Update the question number each time after user submits an answer.
+        // TODO: Ensure randomized questions don't repeat.
+        // TODO: Create an activity that displays the total score at the end of each round.
+
+        try {
+            JSONObject jsonObject = new JSONObject(loadJSONFromAsset(getApplicationContext()));
+
+            JSONArray jsonArrayQuestion =  jsonObject.getJSONArray("questions");
+            JSONObject questionJSONObject  = jsonArrayQuestion.getJSONObject(0);
+
+            String test = questionJSONObject.getString("description");
+
+            textViewQuestionDescription.setText(test);
+            textViewQuestionDescription.setGravity(Gravity.CENTER);
+
+            JSONObject answersJSONObject = questionJSONObject.getJSONObject("answers");
+
+            // Set the text of all four radio buttons with the answers obtained from JSON File
+            radioButton.setText(answersJSONObject.getString("option1"));
+            radioButton2.setText(answersJSONObject.getString("option2"));
+            radioButton3.setText(answersJSONObject.getString("option3"));
+            radioButton4.setText(answersJSONObject.getString("option4"));
+
+            correctAnswer = answersJSONObject.getString("answer");
+
+        } catch (JSONException jsonException) {
+            jsonException.printStackTrace();
+        }
+
+
 
         radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -66,11 +107,42 @@ public class MainQuizActivity extends AppCompatActivity {
         });
     }
 
+    public String loadJSONFromAsset(Context context) {
+        String json;
+        try {
+            InputStream inputStream = context.getAssets().open("quiz-ques-ans.json");
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+
+            json = new String(buffer, "UTF-8");
+        }
+        catch (IOException exception) {
+            exception.printStackTrace();
+            return null;
+        }
+
+        return json;
+    }
+
+
     public void submitAnswer(View view) {
         // Get the reference to the radio button that was selected by the user
         RadioButton selectedAnswer = (RadioButton) findViewById(radioGroup.getCheckedRadioButtonId());
 
-        // Match the answer and see if it is correct
-        Toast.makeText(getApplicationContext(), selectedAnswer.getText(), Toast.LENGTH_SHORT).show();
+        if (selectedAnswer.getText().equals(correctAnswer)) {
+            // Match the answer and see if it is correct
+            soundClip = MediaPlayer.create(this, R.raw.successsound);
+            soundClip.start();
+            Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_SHORT).show();
+            totalScore += 10;
+            Toast.makeText(getApplicationContext(), String.valueOf(totalScore), Toast.LENGTH_SHORT).show();
+        }
+        else {
+            soundClip = MediaPlayer.create(this, R.raw.failsound);
+            soundClip.start();
+            Toast.makeText(getApplicationContext(), "Wrong!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
